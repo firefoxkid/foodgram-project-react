6773478bbd02da876a6a862a05ee27081ec2ad16
firefoxkid <glazov70000@yandex.ru>
2022-04-06 20:44:47 +0400

    06042022

--- foodgram/foodgram/settings.py
+++ foodgram/foodgram/settings.py
@@ -7,9 +7,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 SECRET_KEY = os.getenv("SECRET_KEY", default="very_bad_key")
 
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
+    '51.250.15.194',
     'localhost',
     '127.0.0.1',
     'backend',
