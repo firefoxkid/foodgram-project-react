09cbc1a24e3ae888195a6ffd6605e2c577c541bd
firefoxkid <glazov70000@yandex.ru>
2022-04-08 18:34:39 +0400

    08042022

--- foodgram/init.sh
+++ foodgram/init.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 python manage.py makemigrations
 python manage.py migrate
-python manage.py loadstatic
-export DJANGO_SUPERUSER_PASSWORD=root
-export DJANGO_SUPERUSER_EMAIL=root@root.root
-export DJANGO_SUPERUSER_USERNAME=root
-python manage.py createsuperuser --no-input
+python manage.py collectstatic
+env
+python manage.py createsuperuser --no-input --username $DJANGO_SUPERUSER_USERNAME --email $DJANGO_SUPERUSER_EMAIL
 gunicorn foodgram.wsgi:application --bind 0.0.0.0:8000
