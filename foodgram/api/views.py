abe735be17d2dbeaad34d5f9f244f5603245075f
firefoxkid <glazov70000@yandex.ru>
2022-04-09 11:03:08 +0400

    09042022 with recipes_limit

--- foodgram/api/views.py
+++ foodgram/api/views.py
@@ -137,11 +137,11 @@ class SubscriptionViewSet(CreateDestroyMixin):
     #     self.perform_destroy(instance)
     #     return Response(status=status.HTTP_204_NO_CONTENT)
     def perform_destroy(self, instance):
-        author = get_object_or_404(User, id=int(self.kwargs.get('author_id')))
-        user = self.request.user
-        instance = get_object_or_404(Follow, author=author, user=user)
-        if instance.user == user:
-            instance.delete()
+        author = get_object_or_404(User, pk=self.kwargs.get('author_id'))
+        instance = get_object_or_404(Follow,
+                                     author=author,
+                                     user=self.request.user)
+        instance.delete()
 
 
 class ShoppingListViewSet(CustomShoppingFavoriteMixin):
