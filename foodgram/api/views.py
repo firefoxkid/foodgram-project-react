8d769e87e8e962dd037f735218026de1571d3401
firefoxkid <glazov70000@yandex.ru>
2022-04-09 10:33:04 +0400

    09042022 with recipes_limit

--- foodgram/api/views.py
+++ foodgram/api/views.py
@@ -4,11 +4,12 @@ from django.http import HttpResponse
 from django.shortcuts import get_object_or_404
 from django_filters.rest_framework import DjangoFilterBackend
 from djoser.views import UserViewSet
-from rest_framework import filters, status, viewsets
+# from rest_framework import filters, status, viewsets
+from rest_framework import filters, viewsets
 from rest_framework.decorators import action
 from rest_framework.generics import ListAPIView
 from rest_framework.permissions import AllowAny, IsAuthenticated
-from rest_framework.response import Response
+# from rest_framework.response import Response
 from rest_framework.viewsets import ReadOnlyModelViewSet
 
 from .filters import RecipeFilter
